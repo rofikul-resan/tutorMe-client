b47b409cb1deac0a27762e78ce66ae512a64dfb1
rofikul-resan <resan6231@gmail.com>
2023-09-29 22:42:12 +0600

    add footer section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import NavigationBar from "@/components/NavigationBar";
 import Banner from "./_homeComponents/Banner";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <NavigationBar />
       <Banner />
       home page
+      <Footer />
     </main>
   );
 };
