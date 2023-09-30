c7c476b3634412d4740b169fb1a95ec56d9ad6d4
rofikul-resan <resan6231@gmail.com>
2023-09-30 17:31:42 +0600

    add crouse card view

--- src/app/dashboard/my-class/page.jsx
+++ src/app/dashboard/my-class/page.jsx
@@ -1,5 +1,15 @@
+import EnrollCrouseCard from "./EnrollCrouseCard";
+
 const MyClass = () => {
-  return <div>my class page</div>;
+  return (
+    <div>
+      <div className="my-16 md:w-10/12 mx-auto grid grid-cols-2 gap-10">
+        <EnrollCrouseCard />
+        <EnrollCrouseCard />
+        <EnrollCrouseCard />
+      </div>
+    </div>
+  );
 };
 
 export default MyClass;
